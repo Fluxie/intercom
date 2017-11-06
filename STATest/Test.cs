9f5795d7a191a6ed5e149245b54d65bf9f0d9632
Mikko Rantanen <jubjub@jubjubnest.net>
2017-11-06 15:39:25 +0200

    NUnit hang

---
+++ STATest/Test.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace STATest
+{
+	[SetUpFixture]
+	class SetUp
+	{
+	}
+
+	[TestFixture]
+	class Test
+	{
+		[Test]
+		public void DoTest()
+		{
+			Assert.IsTrue( true );
+		}
+	}
+}
